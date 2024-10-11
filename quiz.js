9c5ac82fd2164dde3b1a953e7578bb14b798160b
encrypt2004 <btech10879.23@bitmesra.ac.in>
2024-10-12 03:17:53 +0530

    i have added several new changes like here i have build a quize cards section

---
+++ quiz.js
@@ -0,0 +1,53 @@
+const questions=[
+    {
+        question:"which is the largest animal in the planet"
+        answers:[
+            {text:"Shark" , correct:false}
+            {text:"Blue whale" , correct:true}
+            {text:"Elephant" , correct:false}
+            {text:"Giraffe" , correct:false}
+            
+        ]
+    },
+      {question:"which is the largest animal in the planet"
+        answers:[
+            {text:"Shark" , correct:false}
+            {text:"Blue whale" , correct:true}
+            {text:"Elephant" , correct:false}
+            {text:"Giraffe" , correct:false}
+            
+        ]
+    },
+    {
+        question:"which is the largest desert in the world"
+        answers:[
+            {text:"kalahari" , correct:false}
+            {text:"Gobi" , correct:true}
+            {text:"Sahara" , correct:false}
+            {text:"Antarctica" , correct:true}
+            
+        ]
+    },
+    {
+        question:"which is the largest animal in the planet"
+        answers:[
+            {text:"Vatican City" , correct:true}
+            {text:"Bhutan" , correct:false}
+            {text:"Nepal" , correct:false}
+            {text:"Shri Lanka" , correct:false}
+            
+        ]
+    },
+    question:"which is the smallest continent in the world ?"
+    answers:[
+        {text:"Asia" , correct:false}
+        {text:"Australia" , correct:true}
+        {text:"Arctic" , correct:false}
+        {text:"Africa" , correct:false}
+        
+    ]
+];
+const questionElement=document.getElementById("question");
+const answerButton=document.getElementById("answer-buttons")
+const nextButton=document.getElementById("next-btn");
+const prevButton=document.getElementById("prev-btn");
